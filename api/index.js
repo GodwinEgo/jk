b514e51ac6e0a13c3518e99360787d7750c74bff
GodwinEgo <godwinehile04@gmail.com>
2023-06-13 02:11:05 +0100

    server

--- api/index.js
+++ api/index.js
@@ -1,54 +1,113 @@
-const express = require("express");
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const multer = require("multer");
-const userRoute = require("./routes/users");
-const authRoute = require("./routes/auth");
-const postRoute = require("./routes/posts");
-const router = express.Router();
-const path = require("path");
+const port = 5000;
 
-dotenv.config();
+// Connect to MongoDB
+mongoose
+  .connect('mongodb://localhost:27017/foodApp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("Connected to MongoDB");
-  }
-);
-app.use("/images", express.static(path.join(__dirname, "public/images")));
+// Define User schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  foodPreferences: [String],
+});
 
-//middleware
+const User = mongoose.model('User', userSchema);
+
 app.use(express.json());
-app.use(helmet());
-app.use(morgan("common"));
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
-
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
+// User registration route
+app.post('/api/register', async (req, res) => {
   try {
-    return res.status(200).json("File uploded successfully");
+    const { name, email, password } = req.body;
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
-    console.error(error);
+    console.error('User registration error:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
+// User login route
+app.post('/api/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-app.listen(8800, () => {
-  console.log("Backend server is running!");
+    // Find the user by email
+    const user = await User.findOne({ email });
+
+    // Check if user exists and password is correct
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      res.status(401).json({ error: 'Invalid email or password' });
+      return;
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user._id }, 'secret_key');
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error('User login error:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Protected route example
+app.get('/api/food', authenticateToken, (req, res) => {
+  const foodData = [
+    { name: 'Pizza', type: 'Italian' },
+    { name: 'Sushi', type: 'Japanese' },
+    { name: 'Burger', type: 'American' },
+  ];
+
+  res.json(foodData);
+});
+
+// Middleware to authenticate JWT token
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
+
+  jwt.verify(token, 'secret_key', (err, user) => {
+    if (err) {
+      res.status(403).json({ error: 'Forbidden' });
+      return;
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
