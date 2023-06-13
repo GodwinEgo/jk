4655b39a37361bd51fa8fd9b42a846c6dc6a93e9
GodwinEgo <godwinehile04@gmail.com>
2023-06-13 02:17:49 +0100

    dash

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,17 @@
 // Dashboard.js
 import React from 'react';
+import FoodPreferences from './FoodPreferences';
+import OrderHistory from './OrderHistory';
+import MoneyBalance from './MoneyBalance';
 
 const Dashboard = () =>
 {
   return (
     <div className="dashboard">
       <h2>User Dashboard</h2>
-      {/* Add components for food preferences, order history, and money balance */ }
+      <FoodPreferences />
+      <OrderHistory />
+      <MoneyBalance />
     </div>
   );
 };
