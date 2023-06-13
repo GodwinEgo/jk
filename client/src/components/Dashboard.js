4c85ff32b06d06298ff83e5e8ecc4efed76ed7fc
GodwinEgo <godwinehile04@gmail.com>
2023-06-13 02:16:30 +0100

    dash

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,45 +1,14 @@
-// Login.js
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+// Dashboard.js
+import React from 'react';
 
-const Login = () =>
+const Dashboard = () =>
 {
-  const [ email, setEmail ] = useState( '' );
-  const [ password, setPassword ] = useState( '' );
-  const history = useHistory();
-
-  const handleSubmit = ( e ) =>
-  {
-    e.preventDefault();
-    // Send login data to the backend
-    // ...
-
-    // Redirect to dashboard after successful login
-    history.push( '/dashboard' );
-  };
-
   return (
-    <div className="login">
-      <h2>Login</h2>
-      <form onSubmit={ handleSubmit }>
-        <input
-          type="email"
-          placeholder="Email"
-          value={ email }
-          onChange={ ( e ) => setEmail( e.target.value ) }
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={ password }
-          onChange={ ( e ) => setPassword( e.target.value ) }
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div className="dashboard">
+      <h2>User Dashboard</h2>
+      {/* Add components for food preferences, order history, and money balance */ }
     </div>
   );
 };
 
-export default Login;
+export default Dashboard;
